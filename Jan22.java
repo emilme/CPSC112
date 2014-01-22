9612d4a6ccb70e00f83e6c66a78b136e33ecbac0
Emily Hong <emily.hong@yale.edu>
2014-01-22 11:48:50 -0500

    Create Jan22.java

---
+++ Jan22.java
@@ -0,0 +1,8 @@
+public class Hello {
+  public static void main(string[] args) {
+  system.out.println("This is a string!");
+  }// end of method
+} //end of class
+
+//Common Syntax errors: missing syntax, too many brackets etc. 
+  
