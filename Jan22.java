166a5433576d6e405d9241b95861c0fb616d3e38
Emily Hong <emily.hong@yale.edu>
2014-01-22 12:09:25 -0500

    Update and rename Jan22.java to Jan22a.java

--- Jan22.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-  public static void main(string[] args) {
-  system.out.println("This is a string!");
-  }// end of method
-} //end of class
-
-//Common Syntax errors: missing syntax, too many brackets etc. 
-  
---
+++ Jan22a.java
@@ -0,0 +1,47 @@
+public class Hello {
+  public static void main(string[] args) {
+  system.out.println("This is a string!");
+  }// end of method
+} //end of class
+
+//Common Syntax errors: missing syntax, too many brackets etc. 
+  
+  public static void printWarning();
+    system.out.println("This product causes cancer.";
+//Fix Redundancy
+
+public class FreshPrince {
+  public static void main(String[] args) {
+    rap(); //Call rap method
+    system.out.println();
+    rap();
+  }
+}
+
+//Program
+
+//Preheat
+  preheat();
+
+//Make Batter
+  makeBatter();
+
+//Bake
+  bake();
+  bake();
+  
+//Decorate
+  decorate();
+
+
+//New Excercise: Defining Methods
+public static void decorate(){
+  system.out.println("Mix ingredients for frosting.");
+  system.out.println("Spread frosting and sprinkles.");
+}
+
+public static void bake(){
+  system.out.println("Set the timer for 8 min.");
+  system.out.println("Place a batch of cookies in the oven.");
+  system.out.println("");
+}
