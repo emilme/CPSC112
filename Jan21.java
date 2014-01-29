caddef51eaa0e2b94960139ba99f31439e33e851
Emily Hong <emily.hong@yale.edu>
2014-01-29 11:36:40 -0500

    Create Jan21.java

---
+++ Jan21.java
@@ -0,0 +1,75 @@
+//January 24, 2013
+//	Word about Style
+//		Structure your code properly
+// 		Eliminate redundancy
+//		Use comments
+//		Use spaces judiciously and consistently 
+//		Indent properly
+//	This ensures interoperability of code going forward
+//
+// Memory: 
+//	* RAM is divided into many cells, each cell has an address.  Each cell has a special # of bits (8, one byte)
+//	* bit can represent 2 values: 0 or 1
+//
+// What do these bits represent?
+//	* 2^8 = 256
+// 1 2 3 4 5  6  7  8
+// 1 2 4 8 16 32 64 128
+// 1 0 0 1 1  0  1  0
+// 1+8+16+64= 89 OR letter Y
+// 
+//
+// Type Systems
+// * category or set of values and operations defined on those values
+// * helps us know what data is 
+//
+// In Java: 
+// Numeric Primitive Data Types:
+// * Integer nubers (byte, short, int, long)
+// 		* Different types = ranges and precisons
+// * Real (floating) numbers (float, double)
+// 		* Can a computer store all real numbers in a range?  No.  Infinite real numbers! 
+// Every data item has a type
+// * "4 is type int"
+// * Real (floating point) literal (e.g. -1.236.12e23) is default type double
+// 
+// Variables - piece of computer memory with a nae and a type
+// 	* like a speed dial
+// 		Steps: 1) Declare, 2) assign values, 3) use
+//
+// 		Syntax: <type> <name>
+// 		ex. 	int x
+// 				x = 3
+			// ** A variable can only store a value of its own type
+			// 
+			// ex.	myGPA = 1.0 + 2.25 (float)
+			//
+			// Questions
+			// * To represent # of students at Yale, which numeric data type variable do you use? 
+			//		Answer: short/int
+			//
+			// * To represent # of World poplation?
+			//		Answer: long (largest number int can store is 2 billion)
+			//
+			// * To represent pi?
+			//		Use double (not float)
+			// 
+			// Patriot Missile Failure 
+			// * Precision of computer error can mess things up
+			//
+			// Java uses Unicode, superset of ASCII (16 bits, 2 byes per character - international)
+//
+// Boolean: represents local value, true or false
+// Reasonable Operations
+	// * each type systemm has its own kind of operations (see slides!!)
+	// ex. char - characters - literal values - compare operations
+// Most operations are intuitive 
+//	Ordering
+
+public class myGPA {
+	public static void main(String)
+}
+
+
+
+
